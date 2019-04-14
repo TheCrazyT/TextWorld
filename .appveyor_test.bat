749b3836f3fe90b61a6c9640730ca1a6575be9c4
CrazyT <TheCrazyT@users.noreply.github.com>
2019-04-14 13:44:51 +0200

    quotes matter ... "<>'

--- .appveyor_test.bat
+++ .appveyor_test.bat
@@ -3,14 +3,14 @@ IF "%MSYS%"=="" (
 	exit /B 1
 )
 echo executing test
-set BASH=%MSYS%\usr\bin\bash -c 'PATH=/usr/bin 
+set BASH=%MSYS%\usr\bin\bash -c "PATH=/usr/bin 
 set PACMAN=%MSYS%\usr\bin\pacman.exe --noconfirm
 
 %PACMAN% -S mingw-w64-x86_64-graphviz
-%BASH% rebaseall -p'
-%BASH% wget -O /tmp/chromedriver_win32.zip https://chromedriver.storage.googleapis.com/73.0.3683.68/chromedriver_win32.zip'
-%BASH% 7z x -y -o/tmp/ chromedriver_win32.zip'
-%BASH% cp /tmp/chromedriver.exe /usr/bin/'
-%BASH% cd dist && PATH_SEPARATOR=: PATH=/usr/bin/ python3.exe -m pip install *.whl'
-%BASH% PATH=/usr/bin/ tree /usr/lib/python3.7'
-%BASH% PATH=/usr/bin/ python3.exe -m nose -sv --with-xunit --with-coverage --cover-xml --cover-html --cover-package textworld'
+%BASH% rebaseall -p"
+%BASH% wget -O /tmp/chromedriver_win32.zip https://chromedriver.storage.googleapis.com/73.0.3683.68/chromedriver_win32.zip"
+%BASH% 7z x -y -o/tmp/ /tmp/chromedriver_win32.zip"
+%BASH% cp /tmp/chromedriver.exe /usr/bin/"
+%BASH% cd dist && PATH_SEPARATOR=: PATH=/usr/bin/ python3.exe -m pip install *.whl"
+%BASH% PATH=/usr/bin/ tree /usr/lib/python3.7"
+%BASH% PATH=/usr/bin/ python3.exe -m nose -sv --with-xunit --with-coverage --cover-xml --cover-html --cover-package textworld"
