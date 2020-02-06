cdf8b3c520443a2de5771560e44d55f5760aa472
TheCrazyT <TheCrazyT@users.noreply.github.com>
2020-02-06 19:54:21 +0100

    fixing certificates

--- .appveyor_init.bat
+++ .appveyor_init.bat
@@ -13,14 +13,15 @@ Set PATHBCK=%PATH%
 REM %BASH% pacman --noconfirm -R $\(PATH=/usr/bin pacman -Sl|PATH=/usr/bin grep mingw\)'
 %PACMAN% -Syuu
 %PACMAN% -Syuu
-%PACMAN% -S rebase tree wget python3 python3-setuptools msys/gcc p7zip ncurses-devel python libffi-devel libffi make msys2-runtime python3-pip automake gcc-libs libcrypt libcrypt-devel zlib zlib-devel gcc-fortran
+%PACMAN% -S rebase tree wget python3 python3-setuptools msys/gcc p7zip ncurses-devel python libffi-devel libffi make msys2-runtime python3-pip automake gcc-libs libcrypt libcrypt-devel zlib zlib-devel gcc-fortran mingw-w64-x86_64-ca-certificates
 %BASH% python3.exe -m pip install --upgrade pip'
+%PIP% install cffi
 %BASH% python3.exe -m pip install --upgrade --force-reinstall numpy'
 %BASH% python3.exe -m pip install pillow --global-option=build_ext --global-option=--disable-jpeg'
 %BASH% wget -nc -q https://github.com/scipy/scipy/releases/download/v1.2.1/scipy-1.2.1-cp37-cp37m-win_amd64.whl'
 %BASH% cp scipy-1.2.1-cp37-cp37m-win_amd64.whl /tmp/scipy-1.2.1-cp37-cp37m-msys_2_11_2_x86_64.whl'
 %BASH% python3.exe -m pip install -v /tmp/scipy-1.2.1-cp37-cp37m-msys_2_11_2_x86_64.whl'
 %BASH% ls /tmp'
-%PIP% install cffi nose wheel
+%PIP% install nose wheel
 
 Set PATH=%PATHBCK%
