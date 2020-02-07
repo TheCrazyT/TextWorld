f79796154573e2c7b508b1fd838dbdef54e3bd1b
TheCrazyT <TheCrazyT@users.noreply.github.com>
2020-02-07 13:12:21 +0100

    Update .appveyor_init.bat

--- .appveyor_init.bat
+++ .appveyor_init.bat
@@ -16,7 +16,7 @@ REM %BASH% pacman --noconfirm -R $\(PATH=/usr/bin pacman -Sl|PATH=/usr/bin grep 
 %PACMAN% -S rebase tree wget python3 python3-setuptools msys/gcc p7zip ncurses-devel python libffi-devel libffi make msys2-runtime python3-pip automake gcc-libs libcrypt libcrypt-devel zlib zlib-devel gcc-fortran mingw-w64-x86_64-ca-certificates
 echo SCIPYURL: %SCIPYURL%
 
-%BASH% wget -nc -q %SCIPYURL%'
+%BASH% wget -nc -nv %SCIPYURL%'
 %BASH% %PYTHON_EXE% -m pip install --upgrade pip'
 %BASH% %PYTHON_EXE% -m pip install cffi'
 %BASH% %PYTHON_EXE% -m pip install --upgrade --force-reinstall numpy'
