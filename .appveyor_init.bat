0624f3e155e8f61b6bb7317005c4eda7020e2742
TheCrazyT <TheCrazyT@users.noreply.github.com>
2020-02-07 12:50:13 +0100

    Update .appveyor_init.bat

--- .appveyor_init.bat
+++ .appveyor_init.bat
@@ -14,11 +14,14 @@ REM %BASH% pacman --noconfirm -R $\(PATH=/usr/bin pacman -Sl|PATH=/usr/bin grep 
 %PACMAN% -Syuu
 %PACMAN% -Syuu
 %PACMAN% -S rebase tree wget python3 python3-setuptools msys/gcc p7zip ncurses-devel python libffi-devel libffi make msys2-runtime python3-pip automake gcc-libs libcrypt libcrypt-devel zlib zlib-devel gcc-fortran mingw-w64-x86_64-ca-certificates
+echo SCIPYURL: %SCIPYURL%
+
+%BASH% wget -nc -q %SCIPYURL%'
 %BASH% %PYTHON_EXE% -m pip install --upgrade pip'
 %BASH% %PYTHON_EXE% -m pip install cffi'
 %BASH% %PYTHON_EXE% -m pip install --upgrade --force-reinstall numpy'
 %BASH% %PYTHON_EXE% -m pip install pillow --global-option=build_ext --global-option=--disable-jpeg'
-%BASH% wget -nc -q %SCIPYURL%'
+
 %BASH% cp %SCIPYNAME% /tmp/%SCIPYNAME%'
 %BASH% %PYTHON_EXE% -m pip install -v /tmp/%SCIPYNAME%'
 %BASH% ls /tmp'
