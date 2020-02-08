bc74e06a9f3b8fce79ba9de570f40c321d66810f
TheCrazyT <TheCrazyT@users.noreply.github.com>
2020-02-08 20:06:47 +0100

    Update .appveyor_test.bat

--- .appveyor_test.bat
+++ .appveyor_test.bat
@@ -8,7 +8,7 @@ set PACMAN=%MSYS%\usr\bin\pacman.exe --noconfirm
 
 %PACMAN% -S mingw-w64-x86_64-graphviz
 %BASH% rebaseall -p"
-%BASH% wget -O /tmp/chromedriver_win32.zip https://chromedriver.storage.googleapis.com/73.0.3683.68/chromedriver_win32.zip"
+%BASH% wget -O /tmp/chromedriver_win32.zip https://chromedriver.storage.googleapis.com/79.0.3945.36/chromedriver_win32.zip"
 %BASH% 7z x -y -o/tmp/ /tmp/chromedriver_win32.zip"
 %BASH% cp /tmp/chromedriver.exe /usr/bin/"
 %BASH% cd dist && PATH_SEPARATOR=: PATH=/usr/bin/ python3.exe -m pip install *.whl"
