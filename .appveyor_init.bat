cd93a83773b55f35fe8e121f60109757a3904e4c
TheCrazyT <TheCrazyT@users.noreply.github.com>
2020-02-08 11:24:17 +0100

    Update .appveyor_init.bat

--- .appveyor_init.bat
+++ .appveyor_init.bat
@@ -20,19 +20,31 @@ echo PYTHON VERSION:
 %BASH% %PYTHON_EXE% --version
 
 %PACMAN% -S rebase tree wget %PYTHON_PACKAGE% %PYTHON_PACKAGE%-setuptools msys/gcc p7zip ncurses-devel python libffi-devel libffi make msys2-runtime python3-pip automake gcc-libs libcrypt libcrypt-devel zlib zlib-devel gcc-fortran mingw-w64-x86_64-ca-certificates
+
+echo pacman versions:
+%PACMAN% -Q
+
 echo SCIPYURL: %SCIPYURL%
 %BASH% wget -nc -nv %SCIPYURL%'
 %BASH% ls -lah %SCIPYNAME%'
 
 %BASH% %PYTHON_EXE% -m pip install --upgrade pip'
 %BASH% %PYTHON_EXE% -m pip install cffi'
+IF %ERRORLEVEL% NEQ 0 (
+	exit 1
+)
+
+%BASH% %PYTHON_EXE% -m pip install gevent'
+IF %ERRORLEVEL% NEQ 0 (
+	exit 1
+)
 %BASH% %PYTHON_EXE% -m pip install --upgrade --force-reinstall jericho==1.1.8 numpy==1.16.2'
 %BASH% %PYTHON_EXE% -m pip install pillow --global-option=build_ext --global-option=--disable-jpeg'
 
 %PIP% install nose wheel
 
-%BASH% cp scipy-1.2.1-cp37-cp37m-win_amd64.whl /tmp/scipy-1.2.1-cp37-cp37m-msys_2_11_2_x86_64.whl'
-%BASH% python3.exe -m pip install -v /tmp/scipy-1.2.1-cp37-cp37m-msys_2_11_2_x86_64.whl'
+echo pip installed versions:
+%PIP% list
 
 %BASH% %PYTHON_EXE% -c "import wheel.pep425tags as w;print(w.get_supported(\"\")[0][2])" ^> /tmp/suffix && ls -la /tmp/suffix'
 FOR /F "tokens=* usebackq delims=^" %%F IN (%MSYS%\tmp\suffix) DO SET SUFFIX=%%F
