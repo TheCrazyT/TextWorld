2849a522ec42e5c0fcd02f4b59339d2047fb4f03
TheCrazyT <TheCrazyT@users.noreply.github.com>
2020-02-15 15:44:54 +0100

    Update .appveyor_init_cygwin.bat

--- .appveyor_init_cygwin.bat
+++ .appveyor_init_cygwin.bat
@@ -1,6 +1,8 @@
 set CYGWIN_PATH=c:\cygwin
 
 set BASH=%CYGWIN_PATH%\bin\bash.exe --login -c '
+set ASH=%CYGWIN_PATH%\bin\ash.exe -c '
+
 %BASH% wget https://raw.githubusercontent.com/transcode-open/apt-cyg/master/apt-cyg -O /usr/bin/apt-cyg 
 %BASH% chmod +x /usr/bin/apt-cyg'
 %BASH% ls -la /usr/bin/apt-cyg'
@@ -35,6 +37,8 @@ IF %ERRORLEVEL% NEQ 0 (
 IF %ERRORLEVEL% NEQ 0 (
 	exit 1
 )
+
+%ASH% rebaseall -p'
 
 %BASH% %PYTHON_EXE% -m pip install -v jericho==1.1.8 numpy==1.16.2 gym==0.12.1 scipy==1.2.1 cffi nose'
 IF %ERRORLEVEL% NEQ 0 (
