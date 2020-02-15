0fd7a769c07917a782936edb2468f1554759d5cb
TheCrazyT <TheCrazyT@users.noreply.github.com>
2020-02-15 13:25:28 +0100

    Update .appveyor_build.bat

--- .appveyor_build.bat
+++ .appveyor_build.bat
@@ -1,12 +1,6 @@
-IF "%MSYS%"=="" (
-	echo "Variable MSYS not set!"
-	exit /B 1
+IF "%BUILD_MSYS%"=="1" (
+	.appveyor_build_msys.bat
 )
-echo executing build
-Set DISTUTILS_DEBUG=1
-
-%MSYS%\usr\bin\bash.exe -c "PATH=/usr/bin/ uname -a"
-REM %MSYS%\usr\bin\bash.exe -c "PATH=/usr/bin/ sed -i s/shell=True/shell=False/ setup.py"
-REM %MSYS%\usr\bin\bash.exe -c "PATH=/usr/bin/ sed -i s/#check/check/ setup.py"
-REM %MSYS%\usr\bin\bash.exe -c "PATH=/usr/bin/ bash.exe setup.sh"
-%MSYS%\usr\bin\bash.exe -c "PATH=/usr/bin/ python3.exe setup.py -v bdist_wheel 2>&1"
+IF "%BUILD_CYGWIN%"=="1" (
+	.appveyor_build_cygwin.bat
+)
