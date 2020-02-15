9622613496f9044bee7a26f2c9d224545549bde0
TheCrazyT <TheCrazyT@users.noreply.github.com>
2020-02-15 16:24:07 +0100

    Update .appveyor_build_cygwin.bat

--- .appveyor_build_cygwin.bat
+++ .appveyor_build_cygwin.bat
@@ -5,7 +5,7 @@ set BASH=%CYGWIN_PATH%\bin\bash.exe --login -c '
 
 echo %CD%
 %BASH% PATH=/usr/bin/ uname -a'
-%BASH% PATH=/usr/bin/ cd /cygdrive/c/projects/textworld/ && %BASH% ls && %BASH% PATH=/usr/bin/ %PYTHON_EXE% setup.py -v bdist_wheel 2>&1'
+%BASH% PATH=/usr/bin/ cd /cygdrive/c/projects/textworld/ ^&^& pwd ^&^& ls ^&^& PATH=/usr/bin/ %PYTHON_EXE% setup.py -v bdist_wheel 2^>^&1'
 
 IF %ERRORLEVEL% NEQ 0 (
 	exit 1
