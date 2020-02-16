e4d5a487493c33930952c6a2d0242928c49e52a5
TheCrazyT <TheCrazyT@users.noreply.github.com>
2020-02-16 07:54:06 +0100

    Update .appveyor_test_cygwin.bat

--- .appveyor_test_cygwin.bat
+++ .appveyor_test_cygwin.bat
@@ -22,4 +22,4 @@ IF %ERRORLEVEL% NEQ 0 (
 	exit 1
 )
 %BASH% PATH=/usr/bin/ tree /usr/lib/python3.7'
-%BASH% PATH=/usr/bin/ %PYTHON_EXE% -m nose -sv --with-xunit --with-coverage --cover-xml --cover-html --cover-package textworld'
+%BASH% cd /cygdrive/c/projects/textworld ^&^& PATH=/usr/bin/ %PYTHON_EXE% -m nose -sv --with-xunit --with-coverage --cover-xml --cover-html --cover-package textworld'
