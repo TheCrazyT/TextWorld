b50b2456d3baa75f2998f1a9bca0a8c096ef6ee3
TheCrazyT <TheCrazyT@users.noreply.github.com>
2020-02-16 07:51:36 +0100

    Update .appveyor_test_cygwin.bat

--- .appveyor_test_cygwin.bat
+++ .appveyor_test_cygwin.bat
@@ -5,8 +5,21 @@ set BASH=%CYGWIN_PATH%\bin\bash.exe --login -c '
 %BASH% apt-cyg install tree graphviz'
 %BASH% rebaseall -p'
 %BASH% wget -O /tmp/chromedriver_win32.zip https://chromedriver.storage.googleapis.com/79.0.3945.36/chromedriver_win32.zip'
+IF %ERRORLEVEL% NEQ 0 (
+	exit 1
+)
 %BASH% 7z x -y -o/tmp/ /tmp/chromedriver_win32.zip'
+IF %ERRORLEVEL% NEQ 0 (
+	exit 1
+)
 %BASH% cp /tmp/chromedriver.exe /usr/bin/'
+%BASH% ls -lah /cygdrive/c/projects/textworld/dist'
+IF %ERRORLEVEL% NEQ 0 (
+	exit 1
+)
 %BASH% cd /cygdrive/c/projects/textworld/dist ^&^& PATH_SEPARATOR=: PATH=/usr/bin/ %PYTHON_EXE% -m pip install *.whl'
+IF %ERRORLEVEL% NEQ 0 (
+	exit 1
+)
 %BASH% PATH=/usr/bin/ tree /usr/lib/python3.7'
 %BASH% PATH=/usr/bin/ %PYTHON_EXE% -m nose -sv --with-xunit --with-coverage --cover-xml --cover-html --cover-package textworld'
