3091db3d58d342587a1954b9aa17b7e1f64e7946
TheCrazyT <TheCrazyT@users.noreply.github.com>
2020-02-27 18:59:42 +0100

    Update .appveyor_test_msys.bat

--- .appveyor_test_msys.bat
+++ .appveyor_test_msys.bat
@@ -11,6 +11,6 @@ set PACMAN=%MSYS_PATH%\usr\bin\pacman.exe --noconfirm
 %BASH% wget -O /tmp/chromedriver_win32.zip https://chromedriver.storage.googleapis.com/79.0.3945.36/chromedriver_win32.zip"
 %BASH% 7z x -y -o/tmp/ /tmp/chromedriver_win32.zip"
 %BASH% cp /tmp/chromedriver.exe /usr/bin/"
-%BASH% cd dist && PATH_SEPARATOR=: PATH=/usr/bin/ python3.exe -m pip install *.whl"
+%BASH% cd dist && PATH_SEPARATOR=: PATH=/usr/bin/ %PYTHON_EXE% -m pip install *.whl"
 %BASH% PATH=/usr/bin/ tree /usr/lib/python3.7"
-%BASH% PATH=/usr/bin/:/mingw64/bin %PYTHON_EXE% -m nose -sv --with-xunit --with-coverage --cover-xml --cover-html --cover-package textworld ^&^& %PYTHON_EXE% -m nose -v tests/test*"
+%BASH% PATH=/usr/bin/:/mingw64/bin %PYTHON_EXE% -m nose -sv --with-xunit --with-coverage --cover-xml --cover-html --cover-package textworld && %PYTHON_EXE% -m nose -v tests/test*"
